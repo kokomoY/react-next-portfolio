b8477e4202c7529be5e3920483fdcb6e6539297c
kokomoY <ktc24a31e0017@edu.kyoto-tech.ac.jp>
2025-02-01 14:20:14 +0900

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -16,14 +16,14 @@ export default async function Home() {
     <>
       <section className={styles.top}>
         <div>
-          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <h1 className={styles.title}>Yashiki Kokomo</h1>
           <p className={styles.description}>
             私たちは市場をリードしているグローバルテックカンパニーです。
           </p>
         </div>
         <Image
           className={styles.bgimg}
-          src="/aaa.jpeg"
+          src="/img-mv.jpg"
           alt=""
           width={4000}
           height={1200}
